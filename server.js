8754fef2d9560ee1bc4f8f0c6a13d425e472ba15
Guilherme <trexguiluan@hotmail.com>
2020-03-26 15:24:06 -0300

    Usando o modulo express e o nodemon

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
-// Vamos falar de coisas básicas da programação em JS
-console.log('npm')
+// user o express para criar e configurar meu servidor
+const express = require('express')
+const server = express()
+
+// criei uma rota '/'
+// e capturo o pedido do cliente para responder
+
+server.get("/", function(req, res) {
+    return res.send("Resposta do servidor")
+})
+
+// liquei meu servidor na porta 3000
+server.listen(3000)
