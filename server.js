cc6c973b4d0310171a49acce04df11a3faebb53b
Guilherme <trexguiluan@hotmail.com>
2020-03-26 23:23:41 -0300

    Update 5.1

--- server.js
+++ server.js
@@ -2,18 +2,67 @@
 const express = require('express')
 const server = express()
 
+const ideas = [
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+        title: "Cursos de Programação",
+        category: "Estudo",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla molestias saepe quia. ",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+        title: "Exercícios",
+        category: "Saúde",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla molestias saepe quia. ",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+        title: "Meditação",
+        category: "Mentalidade",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla molestias saepe quia. ",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+        title: "Karaokê",
+        category: "Diversão em Família",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla molestias saepe quia. ",
+        url: "https://rocketseat.com.br"
+    },
+]
+
 // configurar arquivos estáticos (css, scripts, imagens)
 server.use(express.static("public"))
+
+//configuração do nunjuncks
+const nunjuncks = require('nunjucks')
+nunjuncks.configure('views', {
+    express: server,
+    noCache: true,
+})
+
 
 // criei uma rota '/'
 // e capturo o pedido do cliente para responder
 
 server.get("/", function(req, res) {
-    return res.sendFile(__dirname + "/index.html")
+
+    let lastIdeas = []
+    for(let idea of ideas){
+        if(lastIdeas.length < 2){
+            lastIdeas.push(idea)
+        }
+    }
+
+    lastIdeas = lastIdeas.reverse()
+
+    return res.render("index.html", { ideas: lastIdeas })
 })
 
 server.get("/ideias", function(req, res) {
-    return res.sendFile(__dirname + "/ideias.html")
+    return res.render("ideias.html")
 })
 
 
